ebcebc829efceb06863ecea195d503713c3e6eb2
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2021-07-02 14:06:41 -0400

    Final Project

---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <LearningResources
+      v-for="res in resources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+      :id="res.id"
+    ></LearningResources>
+  </ul>
+</template>
+
+<script>
+import LearningResources from './LearningResource.vue';
+
+export default {
+  inject: ['resources'],
+  components: {
+    LearningResources
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
